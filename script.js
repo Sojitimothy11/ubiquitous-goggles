50f36feaee772744de8a11f1a0b223f739e0940a
Sojitimothy11 <timothysoji1@gmail.com>
2023-07-30 12:07:34 +0100

    week2-assignment

---
+++ script.js
@@ -0,0 +1,10 @@
+//input current year
+let a=parseInt(prompt("Type current year"));
+//input birth year
+let b=parseInt(prompt("What year are you born"));
+//input function
+function age(a,b){
+  return a-b;
+}
+
+console.log("you are "+(a-b)+" years old");
